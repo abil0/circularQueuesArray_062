e9dde792aa2441cf3042f3e96d7d66c6ac36569c
abil0 <hoenabil6@gmail.com>
2025-06-12 18:39:22 +0700

    tahap 12

--- .cpp
+++ .cpp
@@ -81,15 +81,30 @@ public:
         }
         cout << "\nElements in the queue are ....\n";
 
-        if (FRONT_position <= REAR_position)
+        if(FRONT_position <= REAR_position)
+
         {
+            while(FRONT_position <= REAR_position)
+            {
            cout << queue_array[FRONT_position] << "   ";
            FRONT_position++;
+            }
+        cout << endl;
+        }
+    
+    else
+    {
+        while (FRONT_position <= max -1 )
+        {
+            cout << queue_array[FRONT_position] << "   ";
+            FRONT_position++;
         }
         cout << endl;
-        
-        
     }
+    
+
+   }
+    
 }
 
 
